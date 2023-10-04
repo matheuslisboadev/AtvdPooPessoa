9d06104a3f88d1d29feb2892847ec66c07dc365e
Matheus Lisboa <125702737+matheuslisboadev@users.noreply.github.com>
2023-10-03 23:17:01 -0300

    Update pessoa.java

--- pessoa.java
+++ pessoa.java
@@ -3,10 +3,12 @@ public class pessoa {
     private int idade;
     private String sobrenome;
 
-    public pessoa (String nome,String sobrenome,int idade ) {
+    public pessoa (String nome,String sobrenome,int idade, String cpf, String estadoCivil ) {
         this.nome = nome;
         this.idade = idade;
         this.sobrenome = sobrenome;
+        this.cpf = cpf;
+        this.estadoCivil = estadoCivil;
     }
     
      public String getNome() {
@@ -20,6 +22,12 @@ public class pessoa {
        public String getSobrenome(){
            return sobrenome;
        }
+    public String getCpf(){
+           return cpf;
+       }
+    public String getEstadoCivil(){
+           return estadoCivil;
+       }
      
       public void setNome(String nome) {
         this.nome = nome;
@@ -32,27 +40,39 @@ public class pessoa {
         public void setSobrenome(String sobrenome){
            this.sobrenome = sobrenome;
        }
+     public void setCpf(String cpf){
+           this.cpf = cpf;
+       }
+     public void setEstadoCivil(String estadoCivil){
+           this.estadoCivil = estadoCivil;
+       }
 }
 
 
 classe: principal.java
 public class principal {
  public static void main (String[] args) {
-     pessoa pessoa = new pessoa("Yuri", "Guedes", 77);
+     pessoa pessoa = new pessoa("Yuri", "Guedes", 77, "123.123.123-13", "Solteiro");
      
      System.out.println("Nome: " + pessoa.getNome());
      System.out.println("Sobrenome: " + pessoa.getSobrenome());  
      System.out.println("Idade: " + pessoa.getIdade());
+     System.out.println("Cpf: " + pessoa.getCpf());
+     System.out.println("Estado Civil: " + pessoa.getEstadoCivil());
        
         
         pessoa.setNome("Matheus");
-        pessoa.setsobrenome("Lisboa");
+        pessoa.setSobrenome("Lisboa");
         pessoa.setIdade(19);
+        pessoa.setCpf("777.777.777-77");
+        pessoa.setEstadoCivil("Casado");
         
         
         System.out.println("Nome: " + pessoa.getNome());
         System.out.println("Posição: " + pessoa.getSobrenome());
         System.out.println("Idade: " + pessoa.getIdade());
+        System.out.println("Cpf: " + pessoa.getCpf());
+        System.out.println("Estado Civil: " + pessoa.getEstadoCivil());
         
  }
 }
